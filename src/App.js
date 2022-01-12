c3ac62de8ca32aaaa926c32351fed6241576a534
Kevaljagani <kevaljagani0119@gmail.com>
2022-01-12 22:33:31 +0530

    Opensea API Linked

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import CollectionCard from './components/CollectionCard';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+const [punkListData, setPunkListData] = useState([])
+
+useEffect(() => {
+  const getMyNfts = async () => {
+    const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0xb96ed3E683E9EFC6BA5Ee87a97c185f1051ee2DF&order_direction=asc');
+    console.log(openSeaData.data.assets)
+    setPunkListData(openSeaData.data.assets)
+  }
+
+  return getMyNfts()
+}, [])
   return (
     <div className='app'>
       <Header />
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -6,17 +6,18 @@ function CollectionCard({id, name, traits, image}) {
     return (
         <div className='collectionCard'>
             <img src={image} />
-            <div className='details'></div>
+            <div className='details'>
             <div className='name'>
-                {name}
-                <div className='id'>
-                    .#{id}
-                </div>
+                 {name}
+                    <div className='id'>
+                        .#{id}
+                  </div>
+                
             </div>
-
             <div className='priceContainer'>
                 <img src={weth} className='wethImage' />
                 <div className='price'>{traits[0]?.value}</div>
+            </div>
             </div>
         </div>
     )
