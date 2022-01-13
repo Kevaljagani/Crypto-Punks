e490ab5a57567695455fb5b9c58acbac8d617ac8
Kevaljagani <kevaljagani0119@gmail.com>
2022-01-13 14:35:07 +0530

    cors FIXED

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import PunkList from './components/PunkList';
 import Main from './components/Main'
 
 function App() {
+
 const [punkListData, setPunkListData] = useState([])
 const [selectedPunk, setSelectedPunk] = useState([1])
 useEffect(() => {
   const getMyNfts = async () => {
-    const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0xb96ed3E683E9EFC6BA5Ee87a97c185f1051ee2DF&order_direction=asc');
+    const openSeaData = await axios.get('https://cors-anywhere.herokuapp.com/https://joke-api-strict-cors.appspot.com/jokes/randomhttps://testnets-api.opensea.io/assets?asset_contract_address=0xb96ed3E683E9EFC6BA5Ee87a97c185f1051ee2DF&order_direction=asc');
     console.log(openSeaData.data.assets, "hello")
     setPunkListData(openSeaData.data.assets)
   }
@@ -22,7 +23,7 @@ useEffect(() => {
 
   return (
     <div className='app'>
-      <Header />
+      <Header/>
       {
         punkListData.length > 0 && ( 
         <>
